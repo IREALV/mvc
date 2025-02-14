90de78f3181b586bd0d96472bae24d8a65307c10
JL Bautista <jl.bautistac@gmail.com>
2025-02-14 08:21:58 -0600

    Create User Controller

---
+++ controllers/usersController.js
@@ -0,0 +1,11 @@
+const User = require('../models/users');
+
+// Create a new user
+exports.create = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
